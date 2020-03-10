4a8caaf78132f3750997feaffcb07bd026e1a5c4
Andrew <andrewmatosdiaz@hotmail.com>
2020-03-10 19:04:09 -0300

    added middleware to handle websockets request

---
+++ app/middleware/mid_action_cable.rb
@@ -0,0 +1,13 @@
+class MidActionCable
+	def initialize(app, options = {})
+		@app = app
+	end
+
+	def call(env)
+		if Faye::Websocket::Driver.Websocket?(env)
+			ActionCable.server.call(env)
+		else
+			@app.call(env)
+		end
+	end
+end
